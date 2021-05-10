e8fe006b61ddc2056ec9567a6fde26d3a63b6226
Matthew <elijah2895@hotmail.com>
2021-05-10 10:35:40 -0400

    touchup

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,7 +1,4 @@
 class ReviewSerializer < ActiveModel::Serializer
-    attributes :id, :username, :rating, :comment, :date, :movie
-    belongs_to :movie
-    def movie
-      self.movie
-    end
-  end
+  attributes :id, :username, :rating, :comment, :date, :movie
+   
+end
