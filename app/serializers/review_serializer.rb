177e7abec4ce46a53f881bd5d4aed4657b56771d
Yuri Nguyen <yuringuyen312@gmail.com>
2021-05-10 10:48:40 -0400

    h

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,7 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
     attributes :id, :username, :rating, :comment, :date, :movie
-    belongs_to :movie
-    def movie
-      self.movie
-    end
+    
+   
   end
